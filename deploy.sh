8c92b96f72c31ad9d2235b40b60f6d38f52abfe6
Benjamin Pick <b.pick@yellowtree.de>
2013-07-04 09:40:42 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -58,6 +58,8 @@ echo
 echo "Creating local copy of SVN repo ..."
 svn co $SVNURL $SVNPATH
 
+sleep 5
+
 echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
