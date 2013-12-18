e00dc3195596591cea84e98d4cbcb081df1a9b40
Benjamin Pick <b.pick@yellowtree.de>
2013-12-18 11:26:53 +0100

    update geoip

--- geoip-detect.php
+++ geoip-detect.php
@@ -29,7 +29,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-require_once(dirname(__FILE__) . '/vendor/geoip/geoip/geoipcity.inc');
+require_once(dirname(__FILE__) . '/vendor/geoip/geoip/src/geoipcity.inc');
 
 require_once(dirname(__FILE__) . '/api.php');
 require_once(dirname(__FILE__) . '/filter.php');
