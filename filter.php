5049436df3f472b7d68e3d03774a2d96fafefa3b
Benjamin Pick <b.pick@yellowtree.de>
2013-12-19 11:16:23 +0100

    fix notice

--- filter.php
+++ filter.php
@@ -21,7 +21,10 @@ function geoip_detect_add_verbose_information_to_record($record)
 	}
 	if ($record)
 	{
-		$record->region_name = $GEOIP_REGION_NAME_COPY[$record->country_code][$record->region];
+		if (!empty($record->country_code) && !empty($record->region))
+			$record->region_name = $GEOIP_REGION_NAME_COPY[$record->country_code][$record->region];
+		else
+			$record->region_name = null;
 	}
 
 	return $record;
