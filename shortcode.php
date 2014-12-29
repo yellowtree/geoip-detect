c040b5e12215071f42e1927869288f4a9889b8c0
Benjamin Pick <benjaminpick@github.com>
2014-12-29 21:32:27 +0100

    fix unset variable

--- shortcode.php
+++ shortcode.php
@@ -3,7 +3,7 @@
 add_shortcode('geoip_detect', 'geoip_detect_shortcode');
 function geoip_detect_shortcode($attr)
 {
-	$lang = isset($attr['lang']) ? array_unique(array($lang, 'en')) : null;
+	$locales = isset($attr['lang']) ? array_unique(array($lang, 'en')) : null;
 	$locales = apply_filters('geoip_detect2_locales', $locales);	
 	$userInfo = geoip_detect_get_info_from_current_ip();
 
