d1320f7d131669f1a43bac02477c4a9153477e08
Benjamin Pick <benjaminpick@github.com>
2014-12-30 12:22:04 +0100

    specifying new shortcode by unit tests

--- readme.txt
+++ readme.txt
@@ -58,9 +58,10 @@ To see which property names are supported, refer to the [Plugin Backend](http://
 
 Or, add the plugin shortcode somewhere in the page or post content:
 
-    Wie ist das Wetter in [geoip_detect property="country_name" lang="de"] ?
+    Wie ist das Wetter in [geoip_detect2 property="country.name" lang="de" default="ihrem Land"] ?
 
 The lang property is optional. If it is not specified, the current site language is used.
+The default property is optional. If the specified property is empty, then this default value is shown.
    
 
 #### What is planned to be implemented? ####
--- tests/test-api.php
+++ tests/test-api.php
@@ -39,4 +39,33 @@ class ApiTest extends WP_UnitTestCase_GeoIP_Detect {
 		$this->assertValidGeoIP2Record($record, GEOIP_DETECT_TEST_IP);
 		$this->assertSame(null, $record->country->name);	
 	}
+	
+	function testShortcode() {
+		add_filter('geoip_detect_get_external_ip_adress', 'geoip_detect_get_external_ip_adress_test_set_test_ip', 101);
+		$this->assertEquals(GEOIP_DETECT_TEST_IP, geoip_detect_get_external_ip_adress());
+		
+		$string = do_shortcode('[geoip_detect2 property="country"]');
+		$this->assertNotEmpty($string, '[geoip_detect2 property="country"]', "The Geoip Detect shortcode did not generate any output");
+		$this->assertNotEquals($string, '[geoip_detect2 property="country"]', "The Geoip Detect shortcode does not seem to be called");
+		$this->assertNotContains('<!--', $string, "Geoip Detect shortcode threw an error: " . $string);
+		
+		$string = do_shortcode('[geoip_detect2 property="country"]');
+		$this->assertEquals('Germany', do_shortcode('[geoip_detect2 property="country"]'));
+		$this->assertEquals('Germany', do_shortcode('[geoip_detect2 property="country.name"]'));
+		$this->assertEquals('de', do_shortcode('[geoip_detect2 property="country.isoCode"]'));
+		$this->assertEquals('de', do_shortcode('[geoip_detect2 property="country.iso_code"]')); // ?
+		
+		$this->assertEquals('Deutschland', do_shortcode('[geoip_detect2 property="country" lang="de"]'));
+		
+		$this->assertEquals('default value', do_shortcode('[geoip_detect2 property="country.confidence" default="default value"]'));
+	}
+	
+	function testInvalidShortcode() {
+		$string = do_shortcode('[geoip_detect2 property="INVALID"]');
+		$this->assertContains('<!--', $string, "Geoip Detect Shortcode threw no error in spite of invalid property name: " . $string);
+		$string = do_shortcode('[geoip_detect2 property="city.INVALID"]');
+		$this->assertContains('<!--', $string, "Geoip Detect Shortcode threw no error in spite of invalid property name: " . $string);
+		$string = do_shortcode('[geoip_detect2 property="INVALID" default="here"]');
+		$this->assertContains('here', $string, "Geoip Detect Shortcode does not contain default value: " . $string);
+	}
 }
