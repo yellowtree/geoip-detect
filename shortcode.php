3d28401bac9cbd966e6a327afa2a2184509a8767
Benjamin Pick <benjaminpick@github.com>
2014-12-30 12:12:39 +0100

    shortcode2

--- shortcode.php
+++ shortcode.php
@@ -1,10 +1,11 @@
 <?php
 
 add_shortcode('geoip_detect', 'geoip_detect_shortcode');
+/**
+ * @deprecated 
+ */
 function geoip_detect_shortcode($attr)
 {
-	$locales = isset($attr['lang']) ? array_unique(array($lang, 'en')) : null;
-	$locales = apply_filters('geoip_detect2_locales', $locales);	
 	$userInfo = geoip_detect_get_info_from_current_ip();
 
 	$defaultValue = isset($attr['default']) ? $attr['default'] : ''; 
@@ -15,6 +16,29 @@ function geoip_detect_shortcode($attr)
 	$propertyName = $attr['property'];
 	
 	if (property_exists($userInfo, $propertyName)) {
+		if ($userInfo->$propertyName)
+			return $userInfo->$propertyName;
+		else
+			return $defaultValue;
+	}
+	
+	return $defaultValue . '<!-- GeoIP Detect: Invalid property name. -->';
+}
+
+add_shortcode('geoip_detect2', 'geoip_detect2_shortcode');
+function geoip_detect2_shortcode($attr)
+{
+	$locales = isset($attr['lang']) ? array_unique(array($lang, 'en')) : null;
+	$locales = apply_filters('geoip_detect2_locales', $locales);
+		
+	$userInfo = geoip_detect2_get_info_from_current_ip($locales);
+
+	$defaultValue = isset($attr['default']) ? $attr['default'] : ''; 
+	
+	$propertyName = $attr['property']; 
+	// TODO: support city->isoCode ...
+	
+	if (isset($userInfo, $propertyName)) {
 		if ($userInfo->$propertyName)
 			return $userInfo->$propertyName;
 		else
