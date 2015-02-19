9ab2f8f34a9d9c441cce8958420436701918f59c
Benjamin Pick <benjaminpick@github.com>
2015-02-19 12:35:50 +0100

    Change licence to GPLv3 because geoip2 is Apache 2.0-licenced.

--- geoip-detect.php
+++ geoip-detect.php
@@ -6,8 +6,8 @@ Description: Retrieving Geo-Information using the Maxmind GeoIP (Lite) Database.
 Author: YellowTree (Benjamin Pick)
 Author URI: http://www.yellowtree.de
 Version: 2.1.1
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Text Domain: geoip-detect
 Domain Path: /languages
 */
@@ -17,7 +17,7 @@ Author: Benjamin Pick (b.pick@yellowtree.de)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
