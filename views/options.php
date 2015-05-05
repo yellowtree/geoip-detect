41374fd50eac9c580ca69ad5af7e2ac8c9fd3827
Benjamin Pick <benjaminpick@github.com>
2015-05-05 09:07:30 +0200

    rem notices

--- views/options.php
+++ views/options.php
@@ -49,10 +49,10 @@ $options = $currentSource->getParameterHTML();
 		<input type="hidden" name="action" value="options" />
 		<h3>General Options</h3>
 		<p>
-			<input type="checkbox" name="options[set_css_country]" value="1" <?php if ($options['set_css_country']) { echo 'checked="checked"'; } ?>>&nbsp;<?php _e('Add a country-specific CSS class to the &lt;body&gt;-Tag.', 'geoip-detect'); ?><br />
+			<input type="checkbox" name="options[set_css_country]" value="1" <?php if (!empty($options['set_css_country'])) { echo 'checked="checked"'; } ?>>&nbsp;<?php _e('Add a country-specific CSS class to the &lt;body&gt;-Tag.', 'geoip-detect'); ?><br />
 		</p>
 		<p>
-			<input type="checkbox" name="options[has_reverse_proxy]" value="1" <?php if ($options['has_reverse_proxy']) { echo 'checked="checked"'; } ?>>&nbsp;The server is behind a reverse proxy<em>
+			<input type="checkbox" name="options[has_reverse_proxy]" value="1" <?php if (!empty($options['has_reverse_proxy'])) { echo 'checked="checked"'; } ?>>&nbsp;The server is behind a reverse proxy<em>
 			<span class="detail-box">
 			<?php if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) : ?>
 			<?php printf(__('(With Proxy: %s - Without Proxy: %s)', 'geoip-detect'), $_SERVER['HTTP_X_FORWARDED_FOR'], $_SERVER['REMOTE_ADDR']); ?><br />
