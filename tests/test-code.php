16e31da2797ac8680994274d663f31a1db8ffee5
Benjamin Pick <benjaminpick@github.com>
2015-10-01 12:24:56 +0200

    stub unit test

---
+++ tests/test-code.php
@@ -0,0 +1,19 @@
+<?php
+
+class CodeTest extends WP_UnitTestCase_GeoIP_Detect {
+	
+	function setUp()
+	{
+		parent::setUp();
+	}
+	
+	function tearDown()
+	{
+		parent::tearDown();
+	}
+	
+	function testIfShorttagsAreUsed() {
+		// TODO Check all plugin files if they contain the string "<?="
+		
+	}
+}
