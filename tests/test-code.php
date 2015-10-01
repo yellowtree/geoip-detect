c652f9fcc0612f603cdbb21402613f7dac233bd7
Benjamin Pick <benjaminpick@github.com>
2015-10-01 17:47:25 +0200

    check files for php shortcodes

--- tests/test-code.php
+++ tests/test-code.php
@@ -13,7 +13,16 @@ class CodeTest extends WP_UnitTestCase_GeoIP_Detect {
 	}
 	
 	function testIfShorttagsAreUsed() {
-		// TODO Check all plugin files if they contain the string "<?="
+		$folders = array('data-sources', 'views');
 		
+		$plugin_dir = dirname(dirname(__FILE__));
+		foreach ($folders as $f) {
+			foreach (glob($plugin_dir . '/' . $f . '/**') as $filename) {
+				
+				$code = file_get_contents($filename);
+				$this->assertNotContains('<?= ', $code, 'File ' . $filename . ' contains the shortcode <?= which is not supported in PHP 5.3');
+				$this->assertNotContains('<? ', $code, 'File ' . $filename . ' contains the shortcode <?  which is not supported on all hosts');
+			}
+		}
 	}
 }
