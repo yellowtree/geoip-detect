fe077fd8ca84f26d2ee17866b05441407d7d1f12
Benjamin Pick <benjaminpick@github.com>
2018-01-25 09:34:25 +0100

    Debug detect client ip

--- views/lookup.php
+++ views/lookup.php
@@ -27,7 +27,22 @@ if ($shortcode_options) {
 		<?php echo $current_source->getStatusInformationHTML(); ?>
 		<br />
 		<br />
-	</p>	
+	</p>
+	
+	<h2>Current IP:</h2>
+	<p>
+		Detected client IP: <?php echo geoip_detect2_get_client_ip(); ?><br>
+		External Server IP: <?php echo geoip_detect2_get_external_ip_adress(); ?>
+	</p>
+	<p>
+		REMOTE_ADDR: <?php echo $_SERVER['REMOTE_ADDR']; ?><br>
+		<?php if(!empty($_SERVER["HTTP_X_FORWARDED_FOR"])): ?>
+		HTTP_X_FORWARDED_FOR: <?php echo $_SERVER["HTTP_X_FORWARDED_FOR"]; ?><br>
+		<?php if (!get_option('geoip-detect-has_reverse_proxy')): ?>
+		<i>(Probably you should enable the reverse proxy option.)</i>
+		<?php endif; ?>
+		<?php endif; ?>
+	</p>
 		
 	<h2><?php _e('Test IP Detection Lookup ', 'geoip-detect');?></h2>
 	<form method="post" action="#">
