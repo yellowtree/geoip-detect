702161918ea9520090a1359ae8a09381697ef221
Benjamin Pick <benjaminpick@github.com>
2019-01-18 18:49:16 +0100

    Update the travis/phpunit test script with the help of wp-cli

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,12 @@ require_once 'vendor/autoload.php';
 $_tests_dir = getenv('WP_TESTS_DIR');
 if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // WPCS: XSS ok.
+	exit( 1 );
+}
+
+// Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
@@ -15,6 +21,7 @@ function _manually_load_plugin() {
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
 
 ini_set('error_reporting', ini_get('error_reporting') | E_USER_NOTICE);
