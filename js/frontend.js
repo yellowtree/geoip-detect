8bf8f2576d9884369521e55c51e3203ad684c639
Benjamin Pick <b.pick@yellowtree.de>
2019-04-02 20:51:14 +0200

    Beginning to work on the js - does not work yet.

---
+++ js/frontend.js
@@ -0,0 +1,26 @@
+import Record from './models/record';
+import $ from 'jquery';
+
+let ajaxPromise = null;
+
+function get_info_raw() {
+    if (!ajaxPromise) {
+        // Do Ajax Request only once per page load
+        ajaxPromise = $.ajax(geoip_detect.ajaxurl, {
+            dataType: 'json',
+            type: 'GET',
+            data: {
+                action: 'geoip_detect2_get_info_from_current_ip',
+                locales: locales
+            }
+        });
+    }
+    return ajaxPromise;
+}
+
+export function get_info() {
+    // async or new Promise - Syntax?
+
+
+}
+
---
+++ js/models/record.js
@@ -0,0 +1,56 @@
+
+import lodash_get from 'lodash.get';
+
+
+_get_localized = function(ret, locales) {
+    if (typeof(ret) == 'object' && typeof(ret.names) == 'object') {
+        for (let locale of locales) {
+            if (ret.names[locale]) {
+                return ret.names[locale];
+            }
+        }
+        return '';
+    }
+}
+
+
+class Record {
+    data = {};
+    default_locales = [];
+
+    constructor(data, default_locales) {
+        this.data = data || {};
+        this.default_locales = default_locales || ['en']; 
+    }
+
+    get(prop, default_value) {
+        return this.get_with_locales(prop, this.default_locales, default_value);
+    }
+    
+    
+    get_with_locales(prop, locales, default_value) {
+        // Treat pseudo-property 'name' as if it never existed
+        if (prop.substring(-5) === '.name') {
+            prop = prop.substring(0, -5);
+        }
+
+        let ret = lodash_get(this.data, prop, default_value);
+
+        // Localize property, if possible
+        ret = _get_localized(ret, locales);
+
+        return ret;
+    }
+
+    
+
+    /**
+     * Get error message, if any
+     * @return string Error Message
+     */
+    message() {
+        return lodash_get(this.data, 'extra.message', '');
+    }
+}
+
+export default Record;
