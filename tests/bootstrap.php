57931144a66b644cacc6db780e3f06d39c57aa29
Benjamin Pick <benjaminpick@github.com>
2020-11-17 16:28:21 +0100

    try again

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,7 @@ define('GEOIP_DETECT_DOING_UNIT_TESTS', true);
 require_once dirname( dirname( __FILE__ ) ) . '/vendor/autoload.php';
 $_tests_dir = getenv( 'WP_TESTS_DIR' ) ?: getenv( 'WP_PHPUNIT__DIR' );
 
-if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+if ( !$_tests_dir ) $_tests_dir = dirname( dirname( __FILE__ ) ) . '/vendor/wp-phpunit/wp-phpunit';
 
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // WPCS: XSS ok.
