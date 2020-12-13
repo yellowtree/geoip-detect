c41349165383239181e0d6e1a461c68d9cbd69b6
Benjamin Pick <benjaminpick@github.com>
2020-12-13 17:08:11 +0100

    flag ajax working

--- js/frontend.js
+++ js/frontend.js
@@ -1,72 +1,13 @@
-import Record from './models/record';
-import { getLocalStorage, setLocalStorage } from './localStorageAccess';
-import _ from './lodash.custom';
-import { makeJSONRequest } from './xhr';
 import { do_shortcodes } from './shortcodes';
+import { get_info, options } from './lookup';
 
-if (!window.geoip_detect) {
-    console.error('Geoip-detect: the JS variable window.geoip_detect is missing - this is needed for the options')
-}
-const options = window.geoip_detect.options || {};
-
-let ajaxPromise = null;
-
-function get_info_raw() {
-    if (!ajaxPromise) {
-        // Do Ajax Request only once per page load
-        const url = options.ajaxurl + '?action=geoip_detect2_get_info_from_current_ip'
-
-        ajaxPromise = makeJSONRequest(url);
-    }
-
-    return ajaxPromise;
-}
-
-async function get_info_cached() {
-    let response = false;
-
-    // 1) Load Info from cookie cache, if possible
-    if (options.cookie_name) {
-        response = getLocalStorage(options.cookie_name)
-        if (response && response.extra) {
-            // This might be an error object - cache it anyway
-            return response;
-        }
-    }
-
-    // 2) Get response
-    try {
-        response = await get_info_raw();
-    } catch(err) {
-        response = err.responseJSON || err;
-    }
-
-    // 3) Save info to cookie cache
-    if (options.cookie_name) {
-        setLocalStorage(options.cookie_name, response, options.cookie_duration_in_days * 24 * 60 * 60)
-    }
-
-    return response;
-}
-
-
-export async function get_info() {
-    let response = await get_info_cached();
-
-    if (typeof(response) !== 'object') {
-        console.error('Geoip-detect: Record should be an object, not a ' + typeof(response), response);
-        response = { 'extra': { 'error': response || 'Network error, look at the original server response ...' }};
-    }
-
-    const record = new Record(response, options.default_locales);
-    return record;
-}
 
 async function add_body_classes() {
     const record = await get_info();
 
     if (record.error()) {
         console.error('Geodata Error (could not add CSS-classes to body): ' + record.error());
+        return;
     }
 
     const css_classes = {
--- js/shortcodes.js
+++ js/shortcodes.js
@@ -1,20 +1,58 @@
+import { get_info } from "./lookup";
+
 // Get Options from data-options and json parse them
 function get_options(el) {
-
+    const raw = el.getAttribute('data-options');
+    try {
+        return JSON.parse(raw);
+    } catch(e) {
+        return {};
+    }
 }
 
 async function do_shortcode_normal() {
-    document.getElementsByClassName('js-geoip-detect-shortcode');
+    const elements = document.getElementsByClassName('js-geoip-detect-shortcode');
+    if (!elements.length) return;
+
+    const record = await get_info();
+
+    if (record.error()) {
+        console.error('Geodata Error (could not execute shortcode [geoip_detect2]): ' + record.error());
+        return;
+    }
+
+    Array.from(elements).forEach(el => {
+
+    });
+
 }
 
 async function do_shortcode_flags() {
-    document.getElementsByClassName('js-geoip-detect-flag');
+    const elements = document.getElementsByClassName('js-geoip-detect-flag');
+    if (!elements.length) return;
 
-    'flag-icon-de' // Use options.default if country cannot be detected
+    const record = await get_info();
+
+    if (record.error()) {
+        console.error('Geodata Error (could not configure the flags): ' + record.error());
+        return;
+    }
+
+    let country = record.get('country.iso_code');
+    if (country) {
+        country = country.substr(0, 2).toLowerCase();
+    }
+
+    Array.from(elements)
+        .forEach(el => {
+            const c = country || get_options(el).default;
+            if (c) {
+                el.classList.add('flag-icon-' + c)
+            }
+        });
 }
 
-async function do_shortcodes() {
+export const do_shortcodes = async function do_shortcodes() {
     do_shortcode_normal();
     do_shortcode_flags();
-}
-export const do_shortcodes;
+};
