0c01e0a4be348a2ea7dab7a7d1fc69cd0cf3a0ec
Benjamin Pick <benjaminpick@github.com>
2021-12-13 17:43:28 +0100

    js mc

---
+++ js/lib/check-recursive.js
@@ -0,0 +1,21 @@
+
+let _listener_active = false; // for recursion detection (maybe remove later)
+let _change_counter = 0; 
+
+export function check_recursive_before() {
+    _change_counter++;
+    if (_listener_active || _change_counter > 100) {
+        console.warn('Error: Thats weird! autosave change detected a recursion (' + _change_counter + ')! Please file a bug report about this.');
+        if (process.env.NODE_ENV !== 'production') {
+            debugger;
+        }
+        return false;
+    }
+    _listener_active = true;
+    return true;
+}
+
+
+export function check_recursive_after() {
+    _listener_active = false;
+}
--- js/shortcodes/onchange.js
+++ js/shortcodes/onchange.js
@@ -1,10 +1,8 @@
 
+import { check_recursive_after, check_recursive_before } from "../lib/check-recursive";
 import { isInternalEvent } from "../lib/events";
 import { set_override_with_merge } from "../lookup/override";
 import { get_options } from "./helpers";
-
-let _listener_active = false; // for recursion detection (maybe remove later)
-let _change_counter = 0; // ToDo remove later!
 
 export function init() {
     document.addEventListener('change', event_listener_autosave_on_change, false);
@@ -23,25 +21,18 @@ function event_listener_autosave_on_change(event) {
         const property = get_options(target).property;
         const value = target.value;
 
-        _change_counter++;
-        if (_listener_active || _change_counter > 100) {
-            console.warn('Error: Thats weird! autosave change detected a recursion! Please file a bug report about this.');
-            if (process.env.NODE_ENV !== 'production') {
-                debugger;
-            }
+        if (!check_recursive_before()) {
             return;
-        } else {
-            _listener_active = true;
-
-            if (target.matches('select.js-geoip-detect-country-select')) {
-                const selected = target.options[target.selectedIndex];
-                const isoCode = selected?.getAttribute('data-c');
-                set_override_with_merge('country.iso_code', isoCode.toUpperCase(), {reevaluate: false});
-            }
-            
-            set_override_with_merge(property, value); // might call do_shortcodes etc.
-
-            _listener_active = false;
         }
+
+        if (target.matches('select.js-geoip-detect-country-select')) {
+            const selected = target.options[target.selectedIndex];
+            const isoCode = selected?.getAttribute('data-c');
+            set_override_with_merge('country.iso_code', isoCode.toUpperCase(), {reevaluate: false});
+        }
+        
+        set_override_with_merge(property, value); // might call do_shortcodes etc.
+
+        check_recursive_after();
     }
 }
